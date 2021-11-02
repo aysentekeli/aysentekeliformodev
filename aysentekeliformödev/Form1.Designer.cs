386ecf0c0537351918c6dfe891207dfd09bfabc3
ayşen tekeli <aysentekeli@hotmail.com>
2021-11-02 14:32:03 +0300

    Proje dosyası ekle.

---
+++ aysentekeliformödev/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace aysentekeliformödev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btntimer = new System.Windows.Forms.Timer(this.components);
+            this.oyunTimer = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblpuan = new System.Windows.Forms.Label();
+            this.lblsüre = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btntimer
+            // 
+            this.btntimer.Interval = 1000;
+            this.btntimer.Tick += new System.EventHandler(this.btntimer_Tick);
+            // 
+            // oyunTimer
+            // 
+            this.oyunTimer.Interval = 1000;
+            this.oyunTimer.Tick += new System.EventHandler(this.oyunTimer_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblsüre);
+            this.panel1.Controls.Add(this.lblpuan);
+            this.panel1.Location = new System.Drawing.Point(648, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(140, 136);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblpuan
+            // 
+            this.lblpuan.AutoSize = true;
+            this.lblpuan.Location = new System.Drawing.Point(3, 14);
+            this.lblpuan.Name = "lblpuan";
+            this.lblpuan.Size = new System.Drawing.Size(67, 20);
+            this.lblpuan.TabIndex = 0;
+            this.lblpuan.Text = "Puanınız:";
+
+            // 
+            // lblsüre
+            // 
+            this.lblsüre.AutoSize = true;
+            this.lblsüre.Location = new System.Drawing.Point(4, 38);
+            this.lblsüre.Name = "lblsüre";
+            this.lblsüre.Size = new System.Drawing.Size(82, 20);
+            this.lblsüre.TabIndex = 1;
+            this.lblsüre.Text = "Kalan Süre:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ClientSizeChanged += new System.EventHandler(this.Form1_ClientSizeChanged);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer btntimer;
+        private System.Windows.Forms.Timer oyunTimer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblpuan;
+        private System.Windows.Forms.Label lblsüre;
+    }
+}
+
---
+++ aysentekeliformödev/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace aysentekeliformödev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        Random rnd = new Random();
+        int toplam =0;
+        int saniye = 10;
+
+        Button btn = new Button();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btntimer.Start();
+            oyunTimer.Start();
+        }
+
+        private void btntimer_Tick(object sender, EventArgs e)
+        {
+            
+            btn.Size = new Size(rnd.Next(20, 100), 50);
+            btn.Location = new Point(rnd.Next(this.Width), rnd.Next(this.Height));
+            btn.Text = rnd.Next(10, 100).ToString();
+            btn.Click += Btn_Click;
+            this.Controls.Add(btn);
+                
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            toplam += int.Parse(btn.Text);
+            lblpuan.Text = ($"Puanınız:{toplam}");
+            
+
+
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            btn.Size = panel1.ClientSize;
+            btn.Size = this.ClientSize;
+
+        }
+
+        private void oyunTimer_Tick(object sender, EventArgs e)
+        {
+            saniye = saniye - 1;
+            lblsüre.Text = Convert.ToString(saniye);
+            lblsüre.Text = $"Kalan Süre:{saniye}";
+            if (lblsüre.Text == "Kalan Süre:0")
+            {
+                btntimer.Stop();
+                oyunTimer.Stop();
+                btn.Visible = false;
+
+                string puan = @"Puanlar.txt";
+                FileStream dy = new FileStream(puan, FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter yaz = new StreamWriter(dy);
+                yaz.WriteLine(yaz);
+                yaz.Close();
+            }
+
+
+        }
+    }
+}
